da99fb671d0402598af28c2fd298f9405106d9bf
Razia <nrwpc.stack@gmail.com>
2023-05-30 12:59:43 +0800

    further demo of lambda

--- Intro to Functional Programming/src/programming/FP01Functional.java
+++ Intro to Functional Programming/src/programming/FP01Functional.java
@@ -5,25 +5,41 @@ import java.util.List;
 public class FP01Functional {
 
     public static void main(String[] args) {
-        printAllNumbersInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
+        List<Integer> numbers = List.of(12,9,13,4,6,2,4,12,15);
+        List<String> courses = List.of("Spring", "Spring Boot", "API", "Mircoservices", "AWS", "PCF", "Azure", "Docker", "Kubernetes");
 
-        System.out.println("Print only even numbers:");
-        printEvenNumbersInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
+        System.out.print("Print all numbers: ");
+        printAllInList(numbers);
+
+        System.out.print("\nPrint only even numbers: ");
+        printEvenNumbersInList(numbers);
+
+        System.out.print("\nPrint only odd numbers: ");
+        printOddNumbersInList(numbers);
+
+        System.out.print("\nPrint all courses: ");
+        printAllInList(courses);
     }
 
 //    private static boolean isEven(int number) {
 //        return number % 2 == 0;
 //    }
 
-    private static void printAllNumbersInListFunctional(List<Integer> numbers) {
+    private static void printAllInList(List<?> listing) {
         // using Method Reference
-        numbers.stream().forEach(System.out::println);
+        listing.stream().forEach(i -> System.out.print(i + " | "));
     }
 
-    private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
+    private static void printEvenNumbersInList(List<Integer> numbers) {
         numbers.stream()
                 // .filter(FP01Functional::isEven) // using Method Reference
                 .filter(num -> num%2 == 0) // using Lambda expression
-                .forEach(System.out::println);
+                .forEach(i -> System.out.print(i + " | "));
+    }
+
+    private static void printOddNumbersInList(List<Integer> numbers) {
+        numbers.stream()
+                .filter(num -> num%2 != 0)
+                .forEach(i -> System.out.print(i + " | "));
     }
 }
