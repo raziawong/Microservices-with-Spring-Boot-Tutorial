9354781addec5713d5f69ce2d96437efce620788
Razia <nrwpc.stack@gmail.com>
2023-05-31 11:43:33 +0800

    example to map function

--- Intro to Functional Programming/src/programming/FP01Functional.java
+++ Intro to Functional Programming/src/programming/FP01Functional.java
@@ -17,6 +17,12 @@ public class FP01Functional {
         System.out.print("\nPrint only odd numbers: ");
         printOddNumbersInList(numbers);
 
+        System.out.print("\nPrint square of all even numbers: ");
+        printSquaresOfEvenNumbersInList(numbers);
+
+        System.out.print("\nPrint cube of all odd numbers: ");
+        printCubesOfOddNumbersInList(numbers);
+
         System.out.print("\nPrint all courses: ");
         printAllInList(courses);
 
@@ -25,6 +31,9 @@ public class FP01Functional {
 
         System.out.print("\nPrint all courses with at least 4 letters: ");
         findStringByLengthInList(courses, 4);
+
+        System.out.print("\nPrint number of letters in each course: ");
+        printNumberOfLettersInList(courses);
     }
 
 //    private static boolean isEven(int number) {
@@ -32,7 +41,6 @@ public class FP01Functional {
 //    }
 
     private static void printAllInList(List<?> listing) {
-        // using Method Reference
         listing.stream().forEach(i -> System.out.print(i + " | "));
     }
 
@@ -49,6 +57,20 @@ public class FP01Functional {
                 .forEach(i -> System.out.print(i + " | "));
     }
 
+    private static void printSquaresOfEvenNumbersInList(List<Integer> numbers) {
+        numbers.stream()
+                .filter(num -> num % 2 == 0)
+                .map(num -> num * num)
+                .forEach(i -> System.out.print(i + " | "));
+    }
+
+    private static void printCubesOfOddNumbersInList(List<Integer> numbers) {
+        numbers.stream()
+                .filter(num -> num % 2 != 0)
+                .map(num -> num * num * num)
+                .forEach(i -> System.out.print(i + " | "));
+    }
+
     private static void findStringInList(List<String> strings, String targetStr) {
         strings.stream()
                 .filter(str -> str.contains(targetStr))
@@ -58,6 +80,12 @@ public class FP01Functional {
     private static void findStringByLengthInList(List<String> strings, int length) {
         strings.stream()
                 .filter(str -> str.trim().length() >= length)
+                .forEach(str -> System.out.print(str + " | "));
+    }
+
+    private static void printNumberOfLettersInList(List<String> strings) {
+        strings.stream()
+                .map(str -> str + " " + str.replace(" ", "").length())
                 .forEach(str -> System.out.print(str + " | "));
     }
 }
