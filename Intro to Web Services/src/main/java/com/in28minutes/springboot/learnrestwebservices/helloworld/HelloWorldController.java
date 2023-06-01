25078dc9645fa271b1ca3146452230254cd12c4f
Razia <nrwpc.stack@gmail.com>
2023-06-01 15:36:44 +0800

    demo to path parameters with variables

--- Intro to Web Services/src/main/java/com/in28minutes/springboot/learnrestwebservices/helloworld/HelloWorldController.java
+++ Intro to Web Services/src/main/java/com/in28minutes/springboot/learnrestwebservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.springboot.learnrestwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,8 +14,12 @@ public class HelloWorldController {
     }
 
     @GetMapping(path = "/hello-world-bean")
-    //@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world/path-var/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean("Hello world, " + name);
     }
 }
