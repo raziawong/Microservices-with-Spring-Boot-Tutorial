920a7ac125fb63e19c8c3409032b10a196cd54cb
Razia <nrwpc.stack@gmail.com>
2023-06-01 11:47:19 +0800

    new maven project to demo RESTful web services

---
+++ Intro to Web Services/src/main/java/com/in28minutes/springboot/learnrestwebservices/LearnRestWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.learnrestwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnRestWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnRestWebServicesApplication.class, args);
+	}
+
+}
---
+++ Intro to Web Services/src/test/java/com/in28minutes/springboot/learnrestwebservices/LearnRestWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.learnrestwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnRestWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
